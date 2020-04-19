3461938e7956fe999e22e3ea395028fd232a41cf
ridney2019 <ridneygbs@gmail.com>
2020-04-19 17:50:21 -0300

    Alteração template

--- matricula/src/app/app.component.ts
+++ matricula/src/app/app.component.ts
@@ -7,54 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-}
-
-class Cadastro{
   //Declaração do atributos e lista.
-  private alunos=null;
-  private nome=null;
-  private dataNascimento=null;
-  private sexo=null;
-  private cpf=null;
-  private email=null;
-  private endereco=null;
-  private telefone=null;
-  private cep=null;
-  private cidade=null;
-  private uf=[
-    { uf: 'AC', nome: 'Acre' },
-    { uf: 'AL', nome: 'Alagoas' },
-    { uf: 'AP', nome: 'Amapá' },
-    { uf: 'AM', nome: 'Amazonas' },
-    { uf: 'BA', nome: 'Bahia' },
-    { uf: 'CE', nome: 'Ceará' },
-    { uf: 'DF', nome: 'Distrito Federal' },
-    { uf: 'ES', nome: 'Espirito Santo' },
-    { uf: 'GO', nome: 'Goiás' },
-    { uf: 'MA', nome: 'Maranhão' },
-    { uf: 'MS', nome: 'Mato Grosso do Sul' },
-    { uf: 'MT', nome: 'Mato Grosso' },
-    { uf: 'MG', nome: 'Minas Gerais' },
-    { uf: 'PA', nome: 'Pará' },
-    { uf: 'PB', nome: 'Paraíba' },
-    { uf: 'PR', nome: 'Paraná' },
-    { uf: 'PE', nome: 'Pernambuco' },
-    { uf: 'PI', nome: 'Piauí' },
-    { uf: 'RJ', nome: 'Rio de Janeiro' },
-    { uf: 'RN', nome: 'Rio Grande do Norte' },
-    { uf: 'RS', nome: 'Rio Grande do Sul' },
-    { uf: 'RO', nome: 'Rondônia' },
-    { uf: 'RR', nome: 'Roraima' },
-    { uf: 'SC', nome: 'Santa Catarina' },
-    { uf: 'SP', nome: 'São Paulo' },
-    { uf: 'SE', nome: 'Sergipe' },
-    { uf: 'TO', nome: 'Tocantins' }];
-    private turmas=[
-    {numero: 14, nome:'LpWeb'}
-  ];
   
-  constructor (nome: String, data: Date, sexo: boolean, cpf: Number, email: String,
-     endereco: String, telefone: Number, cep: String, cidade: String ){
+   nome=null;
+   dataNascimento=null;
+   sexo=null;
+   cpf=null;
+   email=null;
+   endereco=null;
+   telefone=null;
+   cep=null;
+   cidade=null;
+   uf=null;
+   turmas=[
+    {numero: 14, nome:'LpWeb'}];
+   alunos=null;
+  
+  constructor (nome: String, data: Date, sexo: Boolean, cpf: Number, email: String,
+     endereco: String, telefone: Number, cep: String, cidade: String, uf: String ){
     this.nome = this.nome;
     this.dataNascimento = this.dataNascimento;
     this.sexo = this.sexo;
@@ -64,6 +34,7 @@ class Cadastro{
     this.telefone = this.telefone;
     this.cep = this.cep;
     this.cidade = this.cidade;
+    this.uf = this.uf;
   }
   
   //Método salvar, adicionar os alunos na lista
@@ -93,5 +64,10 @@ class Cadastro{
       this.cidade = null;
       this.uf = null;
     } 
-  
-  }
+
+}
+
+
+ 
+
+ 
--- matricula/src/app/app.module.ts
+++ matricula/src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { FormsModule } from '@angular/forms'
 import { ChartsModule } from 'ng2-charts';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -11,8 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ChartsModule
+    FormsModule,
+    ChartsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
